47cc5d416ea9d4d663d1ca09306b8138b72ba969
Ichsan <ichsannuriman437@gmail.com>
2022-06-23 11:48:49 +0700

    initial commit

---
+++ components/Info.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Info() {
+    return (
+        <div className="info">
+            <img src="./images/profile-picture.jpg" className="info-img" />
+            <h1>M. Ichsan Nur Iman</h1>
+            <h2>Frontend Developer</h2>
+            <h3>ichsan.website</h3>
+            <div className="info-btn">
+                <img src="./images/email-btn.png" />
+                <img src="./images/linkedin-btn.png" />
+            </div>
+        </div>
+    )
+}
